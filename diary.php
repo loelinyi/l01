00a8894da600485129812f580c2f3151f75ac9dc
loelinyi <loelinyi@outlook.com>
2013-10-27 21:20:05 -0400

    Add pagenition

---
+++ diary.php
@@ -0,0 +1,24 @@
+<div id="content">
+<div class="content_box"><h1>Diary 日记</h1></div>
+<?php
+if ($_GET['y']=="2")
+{
+}
+elseif ($_GET['y']=="1")
+{
+}
+else
+{
+	include "diary_be3tf.txt";
+	include "diary_be3sv.txt";
+	include "diary_be3kz.txt";
+	include "diary_an3ky.txt";
+	include "diary_be3iz.txt";
+	include "diary_be3ix.txt";
+	include "diary_be3ho.txt";
+	include "diary_be3hj.txt";
+	include "diary_be3ez.txt";
+	include "diary_armbh.txt";
+} 
+?>
+</div>
---
+++ picture.php
@@ -0,0 +1,17 @@
+<div id="content">
+<div class="content_box"><h1>Picture 图片</h1></div>
+<?php
+if ($_GET['y']=="2")
+{
+}
+elseif ($_GET['y']=="1")
+{
+}
+else
+{
+	include "picture_ip3tn.txt";
+	include "picture_ip3nk.txt";
+} 
+?>
+</div>
+
