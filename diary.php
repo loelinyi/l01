20d73217bee3f6447b1bbc18ed8a9b64635a5bcb
olp-wzhao <wzhao@liberal.ola.org>
2014-03-26 14:49:48 -0400

    add new diary Shanghai Impression

--- diary.php
+++ diary.php
@@ -6,9 +6,16 @@ if ($_GET['y']=="2")
 }
 elseif ($_GET['y']=="1")
 {
+	echo "<div class=\"page_index_box\" style=\"width:100%\"><div class=\"page_index\" style=\"width: 50%; margin: 0 auto;\"><a href=\"page.php?l=d\" style=\"color: #e9e9e9;\">&nbsp;&lt;&lt;&nbsp;</a>&nbsp;&nbsp;<a href=\"page.php?l=d\" style=\"color: #e9e9e9;\">&nbsp;&lt;&nbsp;</a>&nbsp;&nbsp;Diary 日记&nbsp;&nbsp;<a href=\"page.php?l=d&y=1\" style=\"color: #e9e9e9;\">&nbsp;&gt;&nbsp;</a>&nbsp;&nbsp;<a href=\"page.php?l=d&y=1\" style=\"color: #e9e9e9;\">&nbsp;&gt;&gt;&nbsp;</a></div></div>\n";
+	include "diary_be3hj.txt";
+	include "diary_be3ez.txt";
+	include "diary_armbh.txt";
+	echo "<div class=\"page_index_box\" style=\"width:100%\"><div class=\"page_index\" style=\"width: 50%; margin: 0 auto;\"><a href=\"page.php?l=d\" style=\"color: #e9e9e9;\">&nbsp;&lt;&lt;&nbsp;</a>&nbsp;&nbsp;<a href=\"page.php?l=d\" style=\"color: #e9e9e9;\">&nbsp;&lt;&nbsp;</a>&nbsp;&nbsp;Diary 日记&nbsp;&nbsp;<a href=\"page.php?l=d&y=1\" style=\"color: #e9e9e9;\">&nbsp;&gt;&nbsp;</a>&nbsp;&nbsp;<a href=\"page.php?l=d&y=1\" style=\"color: #e9e9e9;\">&nbsp;&gt;&gt;&nbsp;</a></div></div>\n";
 }
 else
 {
+	echo "<div class=\"page_index_box\" style=\"width:100%\"><div class=\"page_index\" style=\"width: 50%; margin: 0 auto;\"><a href=\"page.php?l=d\" style=\"color: #e9e9e9;\">&nbsp;&lt;&lt;&nbsp;</a>&nbsp;&nbsp;<a href=\"page.php?l=d\" style=\"color: #e9e9e9;\">&nbsp;&lt;&nbsp;</a>&nbsp;&nbsp;Diary 日记&nbsp;&nbsp;<a href=\"page.php?l=d&y=1\" style=\"color: #e9e9e9;\">&nbsp;&gt;&nbsp;</a>&nbsp;&nbsp;<a href=\"page.php?l=d&y=1\" style=\"color: #e9e9e9;\">&nbsp;&gt;&gt;&nbsp;</a></div></div>\n";
+	include "diary_be4gv.txt";
 	include "diary_be4et.txt";
 	include "diary_be3tf.txt";
 	include "diary_be3sv.txt";
@@ -17,9 +24,7 @@ else
 	include "diary_be3iz.txt";
 	include "diary_be3ix.txt";
 	include "diary_be3ho.txt";
-	include "diary_be3hj.txt";
-	include "diary_be3ez.txt";
-	include "diary_armbh.txt";
+	echo "<div class=\"page_index_box\" style=\"width:100%\"><div class=\"page_index\" style=\"width: 50%; margin: 0 auto;\"><a href=\"page.php?l=d\" style=\"color: #e9e9e9;\">&nbsp;&lt;&lt;&nbsp;</a>&nbsp;&nbsp;<a href=\"page.php?l=d\" style=\"color: #e9e9e9;\">&nbsp;&lt;&nbsp;</a>&nbsp;&nbsp;Diary 日记&nbsp;&nbsp;<a href=\"page.php?l=d&y=1\" style=\"color: #e9e9e9;\">&nbsp;&gt;&nbsp;</a>&nbsp;&nbsp;<a href=\"page.php?l=d&y=1\" style=\"color: #e9e9e9;\">&nbsp;&gt;&gt;&nbsp;</a></div></div>\n";
 } 
 ?>
 </div>
